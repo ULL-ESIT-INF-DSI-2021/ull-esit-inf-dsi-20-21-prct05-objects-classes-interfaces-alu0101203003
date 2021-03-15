ed4f1da7bfb8d68956e2c12c8725193482a2f994
alu0101203003 <alu0101203003@ull.edu.es>
2021-03-15 09:39:50 +0000

    comienzo ej pe102

---
+++ tests/pe102.spec.ts
@@ -0,0 +1,10 @@
+import 'mocha';
+import {expect} from 'chai';
+import {} from '../src/pe102';
+
+describe('Modificacion pe102', () => {
+    it(' returns ', () => {
+      expect().to.be.deep.equal();
+    });
+
+});
